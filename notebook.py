e64c008e7490a1fce7065aca3e0f793567b862da
Aditya Khalkar <22510466.dypit@dypvp.edu.in>
2025-01-10 00:18:53 +0530

    Added notebooks

--- notebook.py
+++ notebook.py
@@ -1,3 +1,12 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "marimo",
+#     "wigglystuff==0.1.7",
+#     "numpy==2.2.1",
+# ]
+# ///
+
 import marimo
 
 __generated_with = "0.10.9"
